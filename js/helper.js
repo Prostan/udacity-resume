a09a5c4c1c3064a56003dc19ec67621c741df164
Stan <stan.adamchyk@gmail.com>
2016-07-04 21:37:10 -0700

    first check

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,4 @@
+
 /*
 
 This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
@@ -110,9 +111,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -131,7 +132,7 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
       locations.push(school.location);
@@ -139,7 +140,7 @@ function initializeMap() {
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
       locations.push(job.location);
@@ -233,6 +234,17 @@ function initializeMap() {
   pinPoster(locations);
 
 }
+
+
+var name = "Stan Adamchk";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%dtat%, role");
+
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedRole);
 
 /*
 Uncomment the code below when you're ready to implement a Google Map!
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,2 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Stan Adamchyk";
+$("#main").append(name.replace("Stan", "Volodia"));
