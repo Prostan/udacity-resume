c7ce79a32ec1cdc55ac3fe8e339c55f22ff623a9
Stan <stan.adamchyk@gmail.com>
2016-07-06 22:49:57 -0700

    added jobs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,7 +18,7 @@ var education = {
       "name": "Zhirovichi College",
       "city": "Zhirovichi",
       "degree": "BA",
-      "major": "Mechanical Engineering"
+      "major": "Mechanical Engineering",
       "dates": 2013
     },
     {
@@ -26,53 +26,65 @@ var education = {
       "city": "Minsk",
       "degree": "Masters",
       "major": "Mechanical Engineering",
-      "dates": 2016
-    }
-  ]
-},
-"onlineCourses": [
-  {
-    title: "Web Developer",
-    school: "Udacity",
-    dates: 2016,
-    url: "someUrl"
-  }
-]
-
-var work = {
-  "jobs": [
-    {
-      "emloyer": "Google",
-      "title": "QA Engineer",
-      "location": "Mountain View, CA",
-      "description": "Worked on Enterprice Search and Cloud search projects",
-      "dates": 2013
-    },
-    {
-      "emloyer": "Linkedin",
-      "title": "QA Engineer",
-      "location": "Mountain View, CA",
-      "description": "Worked on Intranet WebApplication and Sales Leeds gen",
-      "dates": 2016
+      "dates": 2016,
+      "url": "https://udacity.com"
     }
   ]
 }
 
 var work = {
+  "jobs": [
+    {
+      "employer": "Google",
+      "title": "QA Engineer",
+      "location": "Mountain View, CA",
+      "description": "Worked on Enterprice Search and Cloud search projects",
+      "dates": "2013 - 2015"
+    },
+    {
+      "employer": "Linkedin",
+      "title": "QA Engineer",
+      "location": "Mountain View, CA",
+      "description": "Worked on Intranet WebApplication and Sales Leeds gen",
+      "dates": "2015 - 2016"
+    }
+  ]
+}
+
+var projects = {
   "projects": [
     {
       "title": "SomeProject",
       "title": "QA Engineer",
       "images": ["someUrl", "someUrl"],
       "description": "Worked on Enterprice Search and Cloud search projects",
-      "dates": 2016
+      "dates": "2013 - 2015"
     },
     {
       "emloyer": "Linkedin",
       "title": "QA Engineer",
       "location": "Mountain View, CA",
       "description": "Worked on Intranet WebApplication and Sales Leeds gen",
-      "dates": 2016
+      "dates": "2015 - 2016"
     }
   ]
+}
+
+for (job in work.jobs) {
+  //Creating new div for work experience
+  $("#workExperience").append(HTMLworkStart);
+  //Conctinate employer and title
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+  // appending to the last element of work-entry
+  $(".work-entry:last").append(formattedEmployerTitle);
+
+  //Same for dates
+  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+  $(".work-entry:last").append(formattedDates);
+
+  //For description
+  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  $(".work-entry:last").append(formattedDescription);
 }
