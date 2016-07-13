39184ada9977574677957ab90f8346235736cb2d
Stan <stan.adamchyk@gmail.com>
2016-07-12 23:28:47 -0700

    google map added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,57 +2,77 @@ var bio = {
   name: "Stan Adamchyk",
   role: "Web Developer",
   contacts: {
-    mobile: "650-555-5555",
+    mobile: "508-283-0758",
     email: "stan.adamchyk@gmail.com",
     github: "@stan",
     location: "Mountain View, CA"
   },
   biopic: "images/profile.jpg",
-  welcomeMessage: "Hello People",
-  skills: ["JavaScript", "UI test automation", "Web Development",
-    "BE test automation", "CSS"]
+  welcomeMessage: "IT Professional is looking for new challenge",
+  skills: ["Web Development, UI test Automation, BE test Automation",
+  "Release managment, Deploiment, Risk managment",
+    "JQure, Angular JS, React, Redux, Backbone", "JavaScript, Java, CSS, HTML, SQL",
+    "WebDriver, TestNg, Maven, Allure, Gradle, LogStash, Git",
+    "MySQL, Cassandra, Micrasoft SQL server"
+  ]
 }
 
 var education = {
   "schools": [
     {
-      "name": "Zhirovichi College",
-      "city": "Zhirovichi, BY",
+      "name": "Zyrovicy College",
+      "location": "Zyrovicy, BY",
       "degree": "BA",
-      "major": "Mechanical Engineering",
+      "major": "ME",
       "dates": 2013,
       "url": "https://udacity.com"
     },
     {
       "name": "BSATU",
-      "city": "Minsk, BY",
+      "location": "Minsk, BY",
       "degree": "Masters",
-      "major": "Mechanical Engineering",
+      "major": "ME",
       "dates": 2016,
       "url": "https://udacity.com"
     }
   ],
-  "onlineCourses": {
+  "onlineCourses": [
+    {
     "title": "WebDeveloper",
-    "shool": "Udacity",
+    "school": "Udacity",
     "dates": "2016",
-    "url": "someUrl"
+    "url": "https://classroom.udacity.com/nanodegrees/nd001"
   }
+  ]
 }
 
 var work = {
   "jobs": [
     {
-      "employer": "Google",
+      "employer": "MokaFive",
       "title": "QA Engineer",
+      "location": "Redwood City, CA",
+      "description": "Enterprice managment system",
+      "dates": "2012 - 2013"
+    },
+    {
+      "employer": "Salesforce.com",
+      "title": "QA Engineer",
+      "location": "San Franciso, CA",
+      "description": "CRM, Chatter",
+      "dates": "2012 - 2013"
+    },
+    {
+      "employer": "Google",
+      "title": "Automation QA Engineer",
       "location": "Mountain View, CA",
       "description": "Enterprice Search and Cloud search test automation",
       "dates": "2013 - 2015"
     },
     {
       "employer": "Linkedin",
-      "title": "QA Engineer",
-      "location": "Mountain View, CA",
+      "title": "QA Engineer, Release Manager",
+      "location": "Sunnyvale, CA",
       "description": "Likedin prodactivity apps test automation",
       "dates": "2015 - 2016"
     },
@@ -73,12 +93,6 @@ var projects = {
       "images": ["images/197x148.gif", "images/197x148.gif"],
       "description": "Worked on Enterprice Search and Cloud search projects",
       "dates": 2015
-    },
-    {
-      "title": "Some Project 2",
-      "images": ["images/197x148.gif", "images/197x148.gif"],
-      "description": "Worked on Intranet WebApplication and Sales Leeds gen",
-      "dates": 2016
     }
   ]
 }
@@ -181,26 +195,46 @@ education.display = function() {
 
     var formattedSchoolName = HTMLschoolName.replace("%data%",
       education.schools[school].name);
-    $(".education-entry:last").append(formattedSchoolName);
-
     var formattedDegree = HTMLschoolDegree.replace("%data%",
       education.schools[school].degree);
-    $(".education-entry:last").append(formattedDegree);
+    var formattedSchoolNameDegree = formattedSchoolName + formattedDegree;
+    $(".education-entry:last").append(formattedSchoolNameDegree);
 
     var formattedDates = HTMLschoolDates.replace("%data%",
       education.schools[school].dates);
     $(".education-entry:last").append(formattedDates);
 
     var formattedLocation = HTMLschoolLocation.replace("%data%",
-      education.schools[school].city);
+      education.schools[school].location);
     $(".education-entry:last").append(formattedLocation);
 
     var formattedMajor = HTMLschoolMajor.replace("%data%",
       education.schools[school].major);
     $(".education-entry:last").append(formattedMajor);
-
-    //TODO add online classes
   }
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+
+    for (course in education.onlineCourses) {
+      var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",
+        education.onlineCourses[course].title);
+      var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",
+        education.onlineCourses[course].school);
+      $(".education-entry:last").append(formattedOnlineTitle +
+        formattedOnlineSchool);
+
+      var formattedOnlineDates = HTMLonlineDates.replace("%data%",
+        education.onlineCourses[course].dates);
+
+      $(".education-entry:last").append(formattedOnlineDates);
+
+      var formattedOnlineUrl = HTMLonlineURL.replace("%data%",
+        education.onlineCourses[course].url);
+      $(".education-entry:last").append(formattedOnlineUrl);
+    }
 }
 
 education.display();
+
+$("#mapDiv").append(googleMap);
+initializeMap();
